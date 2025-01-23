f975637c80d96be193d058d42bc22599710757fd
Santheepkumar <santheepkumar@heady.io>
2025-01-23 21:23:35 +0530

    feat: update key mapping and add shortcut file for ubuntu

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -38,7 +38,6 @@ map("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
 -- greatest remap ever
 map("x", "<leader>p", [["_dP]])
-
 -- next greatest remap ever : asbjornHaland
 map({ "n", "v" }, "<leader>y", [["+y]])
 map("n", "<leader>Y", [["+Y]])
@@ -52,12 +51,7 @@ map("n", "Q", "<nop>")
 map("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 map("n", "<leader>f", vim.lsp.buf.format)
 
-map("n", "<C-k>", "<cmd>cnext<CR>zz")
-map("n", "<C-j>", "<cmd>cprev<CR>zz")
-map("n", "<leader>k", "<cmd>lnext<CR>zz")
-map("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("v", "yo", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Go to the next buffer
